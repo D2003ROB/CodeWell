f88616a255dfa38d78fd5407782937bf369277e3
Petru Trimbitas <petru.trimbitas@yahoo.com>
2020-05-04 15:05:21 +0300

    Configure migrations for DBIx::Class

---
+++ codewellBe/share/migrations/PostgreSQL/deploy/1/001-auto.sql
@@ -0,0 +1,14 @@
+--
+-- Created by SQL::Translator::Producer::PostgreSQL
+-- Created on Mon May  4 14:55:57 2020
+--
+;
+--
+-- Table: users
+--
+CREATE TABLE "users" (
+  "id" text NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+;
