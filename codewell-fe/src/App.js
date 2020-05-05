88f12e22e7ef77bd59d78ee916d8709a5673994e
Petru Trimbitas <petru.trimbitas@yahoo.com>
2020-05-05 19:02:49 +0300

    Add i18n

--- codewell-fe/src/App.js
+++ codewell-fe/src/App.js
@@ -1,26 +1,62 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useTranslation } from 'react-i18next';
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { t } = useTranslation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">{t('welcome')}</Link>
+            </li>
+            <li>
+              <Link to="/about">{t('About')}</Link>
+            </li>
+            <li>
+              <Link to="/users">{t('Users')}</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Users() {
+  return <h3>Users</h3>;
 }
 
 export default App;
